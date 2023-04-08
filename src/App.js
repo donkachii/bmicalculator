90d5135d3713b8b35c6e32fa9c810c14686106fd
Onyekachi Nwakaihe <onyekachi.nwakaihe@africaprudential.com>
2023-04-08 19:38:08 +0100

    Added BMI Inputs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,167 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useRef } from "react";
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar, Input, InputNumber, Radio, Button } from "antd";
+import ColorSlider from "./components/ColorSlider";
 
 function App() {
+  const [bmiValue, setBmiValue] = useState({
+    gender: "male",
+    unit: "metric",
+    name: "",
+    age: null,
+    height: null,
+    weight: null,
+  });
+  const [bmi, setBmi] = useState(null);
+  const [bmiCategory, setBmiCategory] = useState(null);
+  const [bmiDisplay, setBmiDisplay] = useState(false);
+
+  const detailRef = useRef(null);
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setBmiValue({
+      ...bmiValue,
+      [name]: value,
+    });
+  };
+
+  const calculateBMI = () => {
+    setBmiDisplay(true);
+
+    let calculatedBmi;
+
+    if (bmiValue.unit === "metric") {
+      calculatedBmi = bmiValue.weight / Math.pow(bmiValue.height / 100, 2);
+    } else {
+      calculatedBmi = (bmiValue.weight / Math.pow(bmiValue.height, 2)) * 703;
+    }
+
+    setBmi(calculatedBmi.toFixed(1));
+    setBmiCategory(getBmiCategory(calculatedBmi));
+    detailRef?.current.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "start",
+    });
+  };
+
+  const getBmiCategory = (bmi) => {
+    if (bmi < 18.5) {
+      return "Underweight";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+      return "Normal";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+      return "Overweight";
+    } else if (bmi >= 30 && bmi <= 34.9) {
+      return "Obesity Class I";
+    } else if (bmi >= 35 && bmi <= 39.9) {
+      return "Obesity Class II";
+    } else {
+      return "Obesity Class III";
+    }
+  };
+
+  console.log(bmiValue);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className="peakmetric_background">
+        <div className="peakmetric_glass">
+          <article className="dashboard">
+            <div className="user">
+              <Avatar
+                size={{ xs: 40, sm: 40, md: 40, lg: 64, xl: 80, xxl: 100 }}
+                icon={<UserOutlined />}
+              />
+            </div>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <div className="details">
+                <div className="peakmetric_unit">
+                  <p className="gender">Units</p>
+                  <Radio.Group
+                    onChange={onChange}
+                    defaultValue={bmiValue.unit}
+                    buttonStyle="solid"
+                    name="unit"
+                  >
+                    <Radio.Button value="metric">Metric (cm, kg)</Radio.Button>
+                    <Radio.Button value="imperial">
+                      Imperial (in, lbs)
+                    </Radio.Button>
+                  </Radio.Group>
+                </div>
+                <div>
+                  <p className="gender">Gender</p>
+                  <Radio.Group
+                    onChange={onChange}
+                    defaultValue={bmiValue.gender}
+                    name="gender"
+                    buttonStyle="solid"
+                  >
+                    <Radio.Button value="male">Male</Radio.Button>
+                    <Radio.Button value="female">Female</Radio.Button>
+                  </Radio.Group>
+                </div>
+                <Input
+                  name="name"
+                  placeholder="Enter your Name"
+                  value={bmiValue.name}
+                  onChange={onChange}
+                />
+                <InputNumber
+                  min={1}
+                  max={120}
+                  defaultValue={bmiValue.age}
+                  addonAfter="years"
+                  placeholder="How old are you?"
+                  // onChange={onChange}
+                />
+                <Input
+                  min={1}
+                  max={120}
+                  defaultValue={bmiValue.height}
+                  addonAfter={bmiValue.unit === "metric" ? "cm" : "in"}
+                  placeholder="Height*"
+                  name="height"
+                  onChange={onChange}
+                />
+                <Input
+                  min={1}
+                  max={120}
+                  defaultValue={bmiValue.weight}
+                  addonAfter={bmiValue.unit === "metric" ? "kg" : "lbs"}
+                  placeholder="Weight*"
+                  name="weight"
+                  onChange={onChange}
+                />
+
+                {/* <InputNumber
+                name="age"
+                placeholder="How old are you?"
+                value={bmiValue.age}
+                onChange={onChange}
+              /> */}
+
+                <Button type="primary" onClick={calculateBMI}>
+                  Calculate
+                </Button>
+              </div>
+            </form>
+          </article>
+          <article className="peakmetric_main"></article>
+        </div>
+      </section>
+      <section className="circle1"></section>
+      <section className="circle2"></section>
+
+      {bmiDisplay && (
+        <section ref={detailRef} className="peakmetric_mobile_details">
+          <ColorSlider bmiCategory={bmiCategory} />
+          {bmi} {bmiCategory}
+        </section>
+      )}
     </div>
   );
 }
---
+++ src/components/ColorSlider.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const ColorSlider = ({ bmiCategory }) => {
+  const bmiCategoryValues = {
+    underweight: 0,
+    normal: 25,
+    overweight: 50,
+    obesityClass1: 75,
+    obesityClass2: 90,
+    obesityClass3: 100,
+  };
+  const sliderValue = bmiCategory ? bmiCategoryValues[bmiCategory] : 0;
+
+  return (
+    <div>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        value={sliderValue}
+        readOnly
+        style={{
+          background: `linear-gradient(
+          to right,
+          #00ccff 0%,
+          #00ccff 25%,
+          #66ff66 25%,
+          #66ff66 50%,
+          #ffcc00 50%,
+          #ffcc00 75%,
+          #ff6666 75%,
+          #ff6666 100%
+        )`,
+          height: "10px",
+          outline: "none",
+          width: "100%",
+          borderRadius: "5px",
+        }}
+      />
+    </div>
+  );
+};
+
+export default ColorSlider;
